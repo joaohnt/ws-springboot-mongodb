398e4caf810cf0e5239baf7ce9391f4bba875950
joaohnt <jpedrooo.cg@gmail.com>
2024-11-12 21:39:55 -0300

    obtain user by id

--- src/main/java/com/joaozera/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joaozera/workshopmongo/resources/UserResource.java
@@ -5,12 +5,8 @@ import com.joaozera.workshopmongo.dto.UserDTO;
 import com.joaozera.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,5 +22,11 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDTO = list.stream().map(UserDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- src/main/java/com/joaozera/workshopmongo/services/UserService.java
+++ src/main/java/com/joaozera/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.joaozera.workshopmongo.services;
 
 import com.joaozera.workshopmongo.domain.User;
 import com.joaozera.workshopmongo.repository.UserRepository;
+import com.joaozera.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,9 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException(id));
     }
 }
